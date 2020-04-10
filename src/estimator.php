c3b44cffc6801dafbde49d0648d9c450c6d61a17
unknown <imohgenius@yahoo.com>
2020-04-10 18:00:25 +0100

    array change

--- src/estimator.php
+++ src/estimator.php
@@ -1,102 +1,63 @@
 <?php
 
-class Input{
- 
 
-  public $region = array("name"=>"Africa", "avgAge"=>19.7, "avgDailyIncomeInUSD"=>5, "avgDailyIncomePopulation"=>0.71);
- 
-  public $periodType= "days";
-  public $timeToElapse= 58;
-  public $reportedCases= 674;
-  public $population= 66622705;
-  public $totalHospitalBeds= 1380614;
-}
-class Impact{
-  public $currentlyInfected;
-  public $infectionsByRequestedTime;
-  public $severeCasesByRequestedTime;
-  public $hospitalBedsByRequestedTime;
-  public $casesForICUByRequestedTime;
-  public $casesForVentilatorsByRequestedTime;
-  public $dollarsInFlight;
 
-}
+$data= array( "region"=> array("name"=>"Africa", "avgAge"=>19.7, "avgDailyIncomeInUSD"=>5, "avgDailyIncomePopulation"=>0.71),"periodType"=> "days",
+"timeToElapse"=> 58,
+"reportedCases"=> 674,
+"population"=> 66622705,
+"totalHospitalBeds"=> 1380614);
 
-class SevereImpact{
-  public $currentlyInfected;
-  public $infectionsByRequestedTime;
-  public $severeCasesByRequestedTime;
-  public $hospitalBedsByRequestedTime;
-  public $casesForICUByRequestedTime;
-  public $casesForVentilatorsByRequestedTime;
-  public $dollarsInFlight;
-}
-class Output{
-  public $data;
-  public $impact;
-  public $severeImpact;
-}
-
-$data= new Input();
-$impact=new Impact();
-$severeImpact=new SevereImpact();
 
 function periodType($data)
 {
-  switch ($data->periodType) {
+  switch ($data["periodType"]) {
     case "days":
-        return intdiv($data->timeToElapse,3);
+        return intdiv($data["timeToElapse"],3);
     break;
     case "weeks":
-        return intdiv($data->timeToElapse, 3)*7;
+        return intdiv($data["timeToElapse"], 3)*7;
     break;
     case "months":
-        return intdiv($data->timeToElapse, 3)*30;
+        return intdiv($data["timeToElapse"], 3)*30;
     break;
 }
 }
 
 function covid19ImpactEstimator($data)
 {
-  $impact->currentlyInfected=$data->reportedCases*10;
+  $impact["currentlyInfected"]=$data["reportedCases"]*10;
 
-  $severeImpact->currentlyInfected=$data->reportedCases*50;
+  $severeImpact["currentlyInfected"]=$data["reportedCases"]*50;
 
-  $impact->infectionsByRequestedTime=$impact->currentlyInfected * (pow(2,periodType($data)));
-  // die(var_dump(periodType($data)));
-  $severeImpact->infectionsByRequestedTime=$severeImpact->currentlyInfected*pow(2,periodType($data));
+  $impact["infectionsByRequestedTime"]=$impact["currentlyInfected"] * (pow(2,periodType($data)));
+  ;
+  $severeImpact["infectionsByRequestedTime"]=$severeImpact["currentlyInfected"]*pow(2,periodType($data));
 
-  $impact->severeCasesByRequestedTime=$impact->infectionsByRequestedTime*0.15;
+  $impact["severeCasesByRequestedTime"]=$impact["infectionsByRequestedTime"]*0.15;
 
-  $severeImpact->severeCasesByRequestedTime=$severeImpact->infectionsByRequestedTime*0.15;
-  $impact->hospitalBedsByRequestedTime=intval(($data->totalHospitalBeds*0.35)-$impact->severeCasesByRequestedTime);
-  $severeImpact->hospitalBedsByRequestedTime=intval(($data->totalHospitalBeds*0.35)-$severeImpact->severeCasesByRequestedTime);
-  $impact->casesForICUByRequestedTime=$impact->infectionsByRequestedTime*0.05;
-  $severeImpact->casesForICUByRequestedTime=$impact->infectionsByRequestedTime*0.05;
-  $impact->casesForVentilatorsByRequestedTime=$impact->infectionsByRequestedTime*0.02;
-  $severeImpact->casesForVentilatorsByRequestedTime=$impact->infectionsByRequestedTime*0.02;
+  $severeImpact["severeCasesByRequestedTime"]=$severeImpact["infectionsByRequestedTime"]*0.15;
+  $impact["hospitalBedsByRequestedTime"]=intval(($data["totalHospitalBeds"]*0.35)-$impact["severeCasesByRequestedTime"]);
+  $severeImpact["hospitalBedsByRequestedTime"]=intval(($data["totalHospitalBeds"]*0.35)-$severeImpact["severeCasesByRequestedTime"]);
+  $impact["casesForICUByRequestedTime"]=$impact["infectionsByRequestedTime"]*0.05;
+  $severeImpact["casesForICUByRequestedTime"]=$impact["infectionsByRequestedTime"]*0.05;
+  $impact["casesForVentilatorsByRequestedTime"]=intval($impact["infectionsByRequestedTime"]*0.02);
+  $severeImpact["casesForVentilatorsByRequestedTime"]=intval($impact["infectionsByRequestedTime"]*0.02);
 
-  $impact->dollarsInFlight=$impact->infectionsByRequestedTime*0.65*1.5*30;
-  $severeImpact->dollarsInFlight=$severeImpact->infectionsByRequestedTime*0.65*1.5*30;
+  $impact["dollarsInFlight"]=$impact["infectionsByRequestedTime"]*0.65*1.5*30;
+  $severeImpact["dollarsInFlight"]=($severeImpact["infectionsByRequestedTime"]*0.65*1.5*30);
 
-  $newoutput= new Output();
-  $newoutput->data=$data;
-  $newoutput->impact=$impact;
-  $newoutput->severeImpact=$severeImpact;
-  // die(var_dump($newoutput->severeImpact->currentlyInfected));
+  $output= array($data,$impact,$severeImpact);
+ 
+  
 
 
-  echo $newoutput->impact->currentlyInfected . "<br>" ;
-  echo $newoutput->severeImpact->currentlyInfected. "<br>";
-  echo $newoutput->impact->infectionsByRequestedTime . "<br>" ;
-  echo $newoutput->severeImpact->infectionsByRequestedTime. "<br>";
-  echo $newoutput->impact->severeCasesByRequestedTime . "<br>";
-  echo $newoutput->severeImpact->severeCasesByRequestedTime ."<br>";
-  echo $newoutput->impact->hospitalBedsByRequestedTime ."<br>";
-  echo $newoutput->impact->dollarsInFlight . "<br>";
-  return $newoutput;
+  // die(var_dump($output));
+  return $output;
+ 
+
 }
 
 covid19ImpactEstimator($data);
-// echo "$newoutput->impact->currentlyInfected <br>";
+
 ?>
